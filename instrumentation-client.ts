15bd1ad43c662d4434e944a355925560a7c502c5
rgilks <rob.gilks@gmail.com>
2026-06-09 20:19:38 +0100

    Add Sentry instrumentation

---
+++ instrumentation-client.ts
@@ -0,0 +1,20 @@
+import * as Sentry from '@sentry/nextjs';
+
+import { beforeSend, tracesSampleRate } from './sentry.shared.config';
+
+const dsn = process.env.NEXT_PUBLIC_SENTRY_DSN;
+
+if (dsn) {
+  Sentry.init({
+    dsn,
+    environment: process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT ?? process.env.NODE_ENV,
+    release: process.env.NEXT_PUBLIC_SENTRY_RELEASE,
+    sendDefaultPii: false,
+    tracesSampleRate: tracesSampleRate(),
+    beforeSend,
+    replaysSessionSampleRate: 0,
+    replaysOnErrorSampleRate: 0,
+  });
+}
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
